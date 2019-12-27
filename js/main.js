9ae8be0a67e29ca48a59790eb6e61a1b491361df
Sergii Torgalo <serg.t0071@gmail.com>
2019-12-27 22:20:50 +0200

    task

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 /*add*/
 let add = function (x, y) {
   return x + y
-};
+}
 
-console.log(add(100, 5));
+console.log(add(100, 5))
 
 /*  subtraction*/
 let sub = function (x, y) {
   return x - y
-};
+}
 
-console.log(sub(10, 25));
+console.log(sub(10, 25))
 
 /*  multiple*/
 function mul(x, y) {
   return x * y
 }
 
-console.log(mul(10, 5));
+console.log(mul(10, 5))
 
 /*  division*/
 function div(x, y) {
@@ -28,37 +28,36 @@ console.log(div(10, 5));
 /*square*/
 let square = function (x) {
   return x * x
-};
+}
 console.log(square(3));
 
 /*square 2^10*/
-let result = 2;
-let counter = 0;
+let result = 2
+let counter = 0
 while (counter < 10) {
-  result = (result * 2);
-  counter = counter + 1;
+  result = (result * 2)
+  counter = counter + 1
 }
-console.log(result);
-
+console.log(result)
 /*factorial*/
 let factorial = function (x) {
   return x * (x - 1)
-};
-console.log(factorial(3));
+}
+console.log(factorial(3))
 
 /*sin*/
 function sin(a) {
   return Math.sin(a)
 }
 
-console.log(sin(1));
+console.log(sin(1))
 
 /*cos*/
 function cos(x) {
   return Math.cos(x)
 }
 
-console.log(cos(1));
+console.log(cos(1))
 
 
 
